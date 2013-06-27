5cd2d9397daa09b9e62bde1a7ca193fa19387f22
Ajan Jayant <ajanjayant@Ajans-MacBook-Pro.local>
2013-06-27 01:32:49 -0700

    Initial Commit

---
+++ GMv2/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  GMv2
+//
+//  Created by Ajan Jayant on 2013-06-27.
+//  Copyright (c) 2013 Ajan Jayant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
