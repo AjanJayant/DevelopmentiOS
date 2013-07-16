5e379235edd0efdeceeb1e12799142c38a6ffca4
tskimmett <taylor@pubnub.com>
2013-07-16 16:53:09 -0700

    Initial server code commit

---
+++ CardGame/server/CardGameServer/Driver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PNServerTest {
+    class Driver {
+        static void Main(string[] args) {
+            Server.init();
+            Console.WriteLine("Press any key to kill the server...");
+            Console.ReadKey();
+        }
+    }
+}
